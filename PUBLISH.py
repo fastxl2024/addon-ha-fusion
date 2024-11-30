39aac61c6aca0f9a6ad09414e239c0c463007782
fastxl2024 <88684192+fastxl2024@users.noreply.github.com>
2024-11-30 09:18:35 +0100

    Update PUBLISH.py

--- PUBLISH.py
+++ PUBLISH.py
@@ -9,7 +9,7 @@ import subprocess
 
 
 def get_latest_release():
-    url = "https://api.github.com/repos/fastxl2024/ha-fusion/releases/latest"
+    url = "https://api.github.com/repos/fastxl2024/Fusion/releases/latest"
     with urllib.request.urlopen(urllib.request.Request(url)) as response:
         return json.loads(response.read().decode())
 
